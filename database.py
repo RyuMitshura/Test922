a4d1f7cc31ae2a9264ce4dd7057226782ca6b750
RyuMitshura <elspescador@gmail.com>
2025-01-26 08:35:09 +0800

    Main

--- database.py
+++ database.py
@@ -1,5 +1,28 @@
 import sqlite3
 
-connec = sqlite3.connect('School.db')
+conn = sqlite3.connect('School.db')
 
-  
+#Create a cursor
+c = conn.cursor()
+
+#Create a Table
+c.execute("""CREATE TABLE student (
+	first_name text,
+	last_name text,
+	email text
+)
+	""")
+
+
+#Commit our code
+conn.commit()
+
+#Close the connection
+conn.close()
+
+# All the data type 
+#NULL, 
+#INTERGER(Whole number), 
+#REAL(Decimal),
+#TEXT, 
+#BlOB(file)
